e00b0b0007284bb2e855e8c5162c2a4b7289d03d
JPark <park.joonw@gmail.com>
2015-02-02 21:04:22 -0700

    basic page layout and styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,37 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require masonry/jquery.masonry
 //= require turbolinks
 //= require_tree .
+
+
+$(function() {
+
+	$('#fullpage').fadeIn(2400, function() {
+		$('.span1').animate({ left: 60 }, 'slow');
+		$('.span2').fadeIn(3000, function() {
+			$('.arrow').fadeIn(600, function() {});
+		});
+	});
+	$('#animate').animate({ left: 200 }, 'slow');
+
+	var $masonry_container = $('#masonry_container');
+	// initialize
+	$masonry_container.masonry({
+	  columnWidth: 200,
+	  gutterWidth: 0,
+	  itemSelector: '.item'
+
+	});
+
+
+	$('a').click(function() {
+		$("body").css("overflow", "auto");
+		$('html, body').animate({
+			scrollTop: $( $(this).attr('href') ).offset().top
+		}, 500);
+		return false;
+	});
+
+});
